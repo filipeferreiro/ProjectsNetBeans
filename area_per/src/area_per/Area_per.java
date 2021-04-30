15f6b18225c45824f6f3767d8c2990a51b4c04e5
Filipe Ferreiro Pereira <filipeeferreiro@gmail.com>
2021-04-30 14:27:13 -0300

    Fixed bugs
    Fixed the directory bugs

--- area_per/src/area_per/Area_per.java
+++ area_per/src/area_per/Area_per.java
@@ -4,18 +4,38 @@
  * and open the template in the editor.
  */
 package area_per;
-
+import java.util.Scanner;
 /**
  *
  * @author Novo
  */
 public class Area_per {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        /* declaração de variáveis */
+        Scanner entrada;
+        entrada = new Scanner(System.in);
+        float lado1,lado2,area,perimetro;
+        /* entrada do meu nome e turma */
+        System.out.println("Filipe Ferreiro Pereira - 2ºDesenvolvimento de Sistemas");
+        /*entrada das variáveis para a operação */
+        System.out.println("Insira a medida do primeiro lado: ");
+        lado1 = entrada.nextFloat();
+        
+        System.out.println("Insira a medida do segundo lado: ");
+        lado2 = entrada.nextFloat();
+        /* estrutura para a operação e saidas */
+        if(lado1 == lado2){
+        area = lado1 * lado2;
+        perimetro = 2*(lado1 + lado2);
+        System.out.println("Area e perímetro do seu quadrado: ");
+        System.out.println(area + "\n" + perimetro);}
+        else{
+        area = lado1 * lado2;
+        perimetro = 2*(lado1 + lado2);
+        System.out.println("Area e perímetro do seu retângulo: ");
+        System.out.println(area + "\n" + perimetro);}
+        
     }
     
 }
